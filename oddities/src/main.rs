0791b8a4365d83d2c89ffc6b4b08aa241ab44513
Mattéo NADLER CAMPOURCY - Portable <matteo.nadler.campou@gmail.com>
2023-09-26 15:19:38 +0200

    added code for oddities

--- oddities/src/main.rs
+++ oddities/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, prelude::*};
+
+fn make_status(n: i32) -> String {
+    if n % 2 == 0 {
+        format!("{} is even", n)
+    } else {
+        format!("{} is odd", n)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Lecture de stdin");
+    let mut lines = input.lines();
+    lines.next();
+    for line in lines {
+        let n = line.parse::<i32>().expect("Entier");
+        println!("{}", make_status(n));
+    }
 }
