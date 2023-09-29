e0642e2cea95443e8040ab9e5551f86ef861b2c1
Mattéo NADLER CAMPOURCY - Portable <matteo.nadler.campou@gmail.com>
2023-09-29 14:50:16 +0200

    added timeloop

---
+++ timeloop/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::{self, prelude::*};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).expect("Lecture de stdin");
+    let times = input.trim().parse::<i32>().expect("Entier");
+    for i in 0..times {
+        println!("{} Abracadabra", i + 1);
+    }
+}
